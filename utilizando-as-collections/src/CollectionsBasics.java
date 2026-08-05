c28a988259070de3d4560cb539620c6520695ace
Camila Ono <camilaono.com@gmail.com>
2026-08-05 15:19:22 -0300

    Collections e principais funções

---
+++ utilizando-as-collections/src/CollectionsBasics.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionsBasics {
+    public static void main(String[] args) {
+
+        /*
+        ----- Não prático -----
+        String nomes[] = {"Camila", "Carina", "Mauricio"};
+        String idades[] = new String[3];
+        */
+
+        // ArrayList é criado com tamanho 10, por padrão!
+        // Inicializando e declarando um ArrayList
+        List<String> nomesArrayList = new ArrayList<>(){{
+            add("1");
+            add("2");
+            add("3");
+        }};        
+
+        //Get
+        System.out.println(nomesArrayList.get(0));
+        System.out.println(nomesArrayList.get(1));
+        
+        //Insert ou Update
+        nomesArrayList.set(0, "0");
+        nomesArrayList.set(1, "1");
+        nomesArrayList.set(2, "2");
+        nomesArrayList.add("3");
+        System.out.println(nomesArrayList);
+
+        //Delete -  remove e desloca todos os elementos seguintes uma posição para a esquerda.
+        nomesArrayList.remove(1);
+        nomesArrayList.remove("3");
+        System.out.println(nomesArrayList);
+
+        //Sort - ordena
+        Collections.sort(nomesArrayList);
+        System.out.println(nomesArrayList);
+        
+        // Reconstruindo o array
+        nomesArrayList.add("2");
+        nomesArrayList.add("3");
+        nomesArrayList.set(1, "1");
+
+        // Percorrendo um ArrayList
+        // for-each
+        for (String nome : nomesArrayList) {
+            System.out.println(nome);
+        }
+
+        // método iterator (collection) - percorrer o array
+        Iterator<String> it = nomesArrayList.iterator();
+        while (it.hasNext()) {
+            String itemAtual = it.next();
+            System.out.println(itemAtual);
+        }
+
+
+
+
+    }
+}
